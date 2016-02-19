50fe8e7d801ef2c36f049b1939d69a78cbc5002b
s <s>
2016-02-19 15:53:26 +0800

    config path to compile

---
+++ src/bin/common/opj_apps_config.h
@@ -0,0 +1,15 @@
+#include "opj_config_private.h"
+
+/* create opj_apps_config.h for  */
+
+//#define OPJ_HAVE_LIBPNG 0
+//#define OPJ_HAVE_PNG_H 1
+//#define OPJ_HAVE_LIBTIFF 0
+//#define OPJ_HAVE_TIFF_H 0
+
+//#define OPJ_HAVE_LIBLCMS1
+//#define OPJ_HAVE_LIBLCMS2
+//#define OPJ_HAVE_LCMS1_H
+//#define OPJ_HAVE_LCMS2_H
+
+
